6339565fb9c8b897604b034e0f35e47ffec1d503
Alyosha <alyosha91383273@gmail.com>
2026-06-08 23:57:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
--> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) ->\
+ None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,7 +8,7 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-average_rating: float, count_of_ratings: int) -> None:
+ average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -19,7 +19,7 @@ average_rating: float, count_of_ratings: int) -> None:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 total_income += (car.comfort_class * (self.clean_power -
-car.clean_mark)* self.average_rating / self.distance_from_city_center)
+ car.clean_mark)* self.average_rating / self.distance_from_city_center)
                 self.wash_single_car(car)
         return round(total_income, 1)
 
@@ -30,7 +30,7 @@ car.clean_mark)* self.average_rating / self.distance_from_city_center)
 
     def rate_service(self, new_rate: int) -> None:
         new_avg = ((self.average_rating * self.count_of_ratings + new_rate)
-/ (self.count_of_ratings + 1))
+ / (self.count_of_ratings + 1))
         self.count_of_ratings += 1
         self.average_rating = round(new_avg, 1)
 
@@ -38,4 +38,4 @@ car.clean_mark)* self.average_rating / self.distance_from_city_center)
         if self.clean_power <= car.clean_mark:
             return 0.0
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
-* self.average_rating / self.distance_from_city_center, 1)
+ * self.average_rating / self.distance_from_city_center, 1)
